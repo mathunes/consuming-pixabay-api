e3601bd4ad196f33ad2e0769ec7afb5f463fc1e6
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-21 16:08:33 -0300

    Adicao da estilizacao do icone de carregamento

---
+++ src/components/Load/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import loadingIcon from '../../assets/image/loading.gif';
+import './style.css';
+
+const Load = () => (
+    <div className="loading-container">
+        <img src={loadingIcon} alt="Loading" />
+    </div>
+)
