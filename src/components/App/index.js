49899db215b87b58cd44e6d7fdf5851acff5bdf8
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-21 16:03:14 -0300

    Exibicao de mensagem de erro se nao for encontrado imagens

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,18 +2,38 @@ import React, { Component } from 'react';
 import SearchBar from '../SearchBar';
 import searchPhotos from './api';
 import Images from '../Images';
+import Error from '../Error';
 
 export default class App extends Component {
 
   state = {
     images: [],
+    error: false,
+    loading: false
   }
 
   onSearchSubmit = async (text) => {
+    this.setState({
+      loading: true
+    })
+
     const response = await searchPhotos(text);
 
+    if (parseInt(response.data.totalHits) > 0) {
+      this.setState({
+        images: response.data.hits,
+        error: false      
+      })
+
+    } else {
+      this.setState({
+        error: true,
+      })
+      
+    }
+
     this.setState({
-      images: response.data.hits
+      loading: false
     })
     
   }
@@ -28,7 +48,11 @@ export default class App extends Component {
       <div className="App">
 
         <SearchBar onSubmit={this.onSearchSubmit}/>
-        <Images images={this.state.images} />
+
+        {
+          (this.state.error) ? <Error /> : <Images images={this.state.images} />
+        }
+        
 
       </div>
     )
---
+++ src/components/Error/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export default class Error extends Component {
+    render() {
+        return (
+            <div className="error">
+                <h2>Não encontrada :(</h2>
+            </div>
+        )
+    }
+}
