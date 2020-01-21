13e6f49e1b71d43041423b256fff9511fb5c54c3
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-21 15:17:53 -0300

    Imagens carregadas na pagina assim que ela é aberta

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -18,6 +18,11 @@ export default class App extends Component {
     
   }
 
+  componentDidMount() {
+    this.onSearchSubmit('');
+  }
+
+
   render() {
     return (
       <div className="App">
--- src/components/Images/index.js
+++ src/components/Images/index.js
@@ -6,14 +6,12 @@ export default class Images extends Component {
     render() {
         return (
             <div className="container container-img">
-                {/* <div className="container-img-item"> */}
                     {this.props.images.map((image) => {
                         return (
                             <img src={image.largeImageURL} key={image.id} />
                         )
                     }
                     )}
-                {/* </div> */}
             </div>
         )
     }
